83a1908113015e217858cc31a861497896fc40cb
Nalimov <nalimvp@gmail.com>
2020-03-31 02:28:18 +0300

    PHP mailer

--- src/js/script.js
+++ src/js/script.js
@@ -1,81 +1,96 @@
 //carousel
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.png" alt="previous"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.png" alt="next"></button>',
-        responsive: [
-            {
-              breakpoint: 991,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true,
-                cssEase: 'linear',
-                arrows: false
-              }
+  $('.carousel__inner').slick({
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_arrow.png" alt="previous"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/right_arrow.png" alt="next"></button>',
+      responsive: [
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+              cssEase: 'linear',
+              arrows: false
             }
-        ]
-    });
-
-    //tabs
-    $('ul.tab').on('click', 'li:not(.tab__item_active)', function() {
-      $(this)
-        .addClass('tab__item_active').siblings().removeClass('tab__item_active')
-        .closest('div.container').find('div.catalog__item').removeClass('catalog__item_active').eq($(this).index()).addClass('catalog__item_active');
-    });
-
-    function toggleSlide(item) {
-      $(item).each(function(i) {
-        $(this).on('click', function(e){
-          e.preventDefault();
-          $('.card__content').eq(i).toggleClass('card__content_active');
-          $('.card__full-descr').eq(i).toggleClass('card__full-descr_active');
-        })
-      })
-    }
-    toggleSlide('.card__link');
-    toggleSlide('.card__link-back');
-
-    //Modal
-    $('[data-modal=consultation]').on('click', function() {
-      $('.overlay, #consultation').fadeIn();
-    });
-
-    $('.modal__close').on('click', function() {
-      $('.overlay').fadeOut();
-    });
-
-    $('.btn_mini').each(function(i){
-      $(this).on('click', function() {
-        $('#order .modal__descr').text($('.card__title').eq(i).text());
-        $('.overlay, #order').fadeIn();
-      });
-    });
-
-    function ValidateForms(form) {
-      $(form).validate({
-        rules: {
-          name: "required",
-          phone: "required",
-          email: {
-            required: true,
-            email: true
           }
-        },
-        messages: {
-          name: "Обязательное поля!",
-          phone: "Обязательное поля!",
-          email: {
-            required: "Обязательное поле!",
-            email: "Не верно введен email"
-          }
-        }
-      });
-    };
-    ValidateForms('#consultation form');
-    ValidateForms('#consultation-form');
-    ValidateForms('#order form');
-
-    $("input[name=phone]").mask("+7 (999) 999-99-99");
+      ]
   });
+
+  //tabs
+  $('ul.tab').on('click', 'li:not(.tab__item_active)', function() {
+    $(this)
+      .addClass('tab__item_active').siblings().removeClass('tab__item_active')
+      .closest('div.container').find('div.catalog__item').removeClass('catalog__item_active').eq($(this).index()).addClass('catalog__item_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.card__content').eq(i).toggleClass('card__content_active');
+        $('.card__full-descr').eq(i).toggleClass('card__full-descr_active');
+      })
+    })
+  }
+  toggleSlide('.card__link');
+  toggleSlide('.card__link-back');
+
+  //Modal
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn();
+  });
+
+  $('.modal__close').on('click', function() {
+    $('.overlay, #order, #consultation, #thanks').fadeOut();
+  });
+
+  $('.btn_mini').each(function(i){
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.card__title').eq(i).text());
+      $('.overlay, #order').fadeIn();
+    });
+  });
+
+  function ValidateForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: "Обязательное поля!",
+        phone: "Обязательное поля!",
+        email: {
+          required: "Обязательное поле!",
+          email: "Не верно введен email"
+        }
+      }
+    });
+  };
+  ValidateForms('#consultation form');
+  ValidateForms('#consultation-form');
+  ValidateForms('#order form');
+
+  $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function(){
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn();
+      $('form').trigger('reset')
+    });
+    return false;
+  });
+});
